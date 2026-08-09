fbdf522ef562dcd5fe1045f0349073b5f77d5f8a
zxtprivate <50144235+zxtprivate@users.noreply.github.com>
2026-08-09 14:56:25 +0800

    chore: change dev server port to 5188

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '127.0.0.1',
-    port: 5173,
+    port: 5188,
     proxy: {
       '/api': 'http://127.0.0.1:8787',
     },
